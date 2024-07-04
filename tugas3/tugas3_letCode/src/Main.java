027143ffbedb0079938d5ce92bbeacf770f26b49
aliffaidz2005 <alif.22315005@umkendari.ac.id>
2024-07-04 21:06:42 +0700

    tugas3 selesai

--- tugas3/tugas3_letCode/src/Main.java
+++ tugas3/tugas3_letCode/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         int [] nums1 = {1,2};
         int [] nums2 = {2};
-         double hasil = findMedianSortedArrays(nums1,nums2);
+        double hasil = findMedianSortedArrays(nums1,nums2);
         System.out.println("hasil : " + hasil);
     }
     public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
