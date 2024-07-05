7f7917dbfa84238eb84e312d2289b714182165bd
aliffaidz2005 <alif.22315005@umkendari.ac.id>
2024-07-05 11:05:59 +0700

    tugasnya saya satukan dalam satu class

--- tugas3/Solution.java
+++ tugas3/Solution.java
@@ -8,6 +8,35 @@ public class Solution {
         int target = 9;
         int[] result = twoSum(nums, target);
         System.out.println("Indices: " + result[0] + ", " + result[1]);
+
+        // tugas let code ke dua find media of two arrray
+        int[] nums1 = new int[]{1, 2};
+        int[] nums2 = new int[]{2};
+        double hasil = findMedianSortedArrays(nums1, nums2);
+        System.out.println("hasil : " + hasil);
+
+    }
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        int[] merged = new int[n1 + n2];
+        int i = 0;
+        int j = 0;
+
+        int k;
+        for(k = 0; i < n1 && j < n2; merged[k++] = nums1[i] <= nums2[j] ? nums1[i++] : nums2[j++]) {
+        }
+
+        while(i < n1) {
+            merged[k++] = nums1[i++];
+        }
+
+        while(j < n2) {
+            merged[k++] = nums2[j++];
+        }
+
+        int mid = merged.length / 2;
+        return merged.length % 2 == 0 ? (double)(merged[mid - 1] + merged[mid]) / 2.0 : (double)merged[mid];
     }
 
     public static int[] twoSum(int[] nums, int target) {
