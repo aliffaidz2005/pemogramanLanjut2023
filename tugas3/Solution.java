6f11801a0e6437be27b385d2ebb426b6946418f6
aliffaidz2005 <153579960+aliffaidz2005@users.noreply.github.com>
2024-07-05 11:12:41 +0700

    tugas 3
    tugas 3 letCode

---
+++ tugas3/Solution.java
@@ -0,0 +1,27 @@
+public class Solution {
+    public Solution() {
+    }
+
+    public static void main(String[] args) {
+        Solution ts = new Solution();
+        int[] nums = new int[]{2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+        System.out.println("Indices: " + result[0] + ", " + result[1]);
+
+        // tugas let code ke dua find media of two arrray
+
+
+    }
+    public static int[] twoSum(int[] nums, int target) {
+        for(int i = 0; i < nums.length; ++i) {
+            for(int j = i + 1; j < nums.length; ++j) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
